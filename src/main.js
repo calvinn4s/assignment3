840873d30078a275d0a71efe3da52d6b21e6f62e
calvinn4s <zoyacalvinna@gmail.com>
2025-06-05 18:12:57 +0700

    fix url2

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#typed", {
+  strings: [
+    "Zoya Calvinna Suladi",
+    "Seorang Web Developer",
+    "Juga Fullstack Developer",
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true,
+});
